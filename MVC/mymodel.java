b804b7216cb23d312b96e9f4f9d58f93260a79bc
SIV <fathersson1302@gmail.com>
2023-07-25 23:15:50 +0530

    Create mymodel.java

---
+++ MVC/mymodel.java
@@ -0,0 +1,20 @@
+
+
+class mymodel
+{
+     String name;
+     String branch;
+     
+     mymodel(String name, String branch)
+     {
+          this.name=name;
+          this.branch=branch;
+          
+          
+     }
+
+     mymodel getvalue()
+     {
+           return this;
+     }
+}
